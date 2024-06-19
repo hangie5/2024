43a584d25d663f367feda96801b2389038401712
hangie5 <142239368+hangie5@users.noreply.github.com>
2024-06-19 19:10:07 +0200

    Add files via upload

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strcat.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjlin <tjlin@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/06/17 17:47:40 by tjlin         #+#    #+#                 */
+/*   Updated: 2024/06/17 18:11:52 by tjlin         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+// int main(void)
+// {
+//     char src[] = "good";
+//     char dest[] = "morning";
+//     printf("%s", ft_strcat(src, dest));
+// }
