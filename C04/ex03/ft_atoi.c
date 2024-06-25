6516d28b260ac1169f25c0557c468905dba44aca
hangie5 <142239368+hangie5@users.noreply.github.com>
2024-06-25 13:16:44 +0200

    Add files via upload

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_atoi.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjlin <tjlin@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/06/19 19:11:02 by tjlin         #+#    #+#                 */
+/*   Updated: 2024/06/23 15:07:44 by tjlin         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	spaces(char *str, int *ptr_i)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == 43 || str[i] == 45)
+	{
+		if (str[i] == 45)
+			count *= -1;
+		i++;
+	}
+	*ptr_i = i;
+	return (count);
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	result = 0;
+	sign = spaces(str, &i);
+	while (str[i] && str[i] >= 48 && str[i] <= 57)
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	result *= sign;
+	return (result);
+}
+
+// int main(void)
+// {
+// 	char *s = "  ---+--+1234ab56789";
+// 	printf ("%d", ft_atoi(s));
+// }
